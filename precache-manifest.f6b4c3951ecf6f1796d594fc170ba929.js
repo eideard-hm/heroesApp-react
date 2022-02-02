ba95fdec2450aa032b092b76d44c70e5608e94b4
eideard-hm <ehhernandez81@misena.edu.co>
2022-02-02 14:49:32 -0500

    Updates

---
+++ precache-manifest.f6b4c3951ecf6f1796d594fc170ba929.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bd7c0d96009e567637f99f4a836d229",
+    "url": "/heroesApp-react/index.html"
+  },
+  {
+    "revision": "bd24e6dd808273efc01b",
+    "url": "/heroesApp-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6c3bbdd8603dfc3f68ef",
+    "url": "/heroesApp-react/static/js/2.87782943.chunk.js"
+  },
+  {
+    "revision": "bd24e6dd808273efc01b",
+    "url": "/heroesApp-react/static/js/main.9b9187fa.chunk.js"
+  },
+  {
+    "revision": "1ab845ceb0b8d0e6b29f",
+    "url": "/heroesApp-react/static/js/runtime~main.8882e275.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.92788dcdd43c9a9377b4ce78882c50d5.js"
+  "/heroesApp-react/precache-manifest.f6b4c3951ecf6f1796d594fc170ba929.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/heroesApp-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
